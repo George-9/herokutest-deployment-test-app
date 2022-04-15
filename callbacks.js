6ef4f579599d0711d84a3f51da77ba872dadcde0
George <georgemuigainjau@gmail.com>
2022-04-15 11:57:55 +0300

    updated websites' data

--- callbacks.js
+++ callbacks.js
@@ -3,7 +3,7 @@ const MongoClient = require("mongodb").MongoClient;
 var axios = require("axios");
 
 const homepage = (_, resp) => {
-  resp.redirect('./home.html')
+  resp.redirect('./homepage/home.html')
 };
 
 const getict = (_, resp) => {
@@ -41,7 +41,6 @@ const getict = (_, resp) => {
 
 
 const getbookshop = (_, resp) => {
-  // resp.contentType('application/json');
 
   var data = JSON.stringify({
     collection: "library_data",
@@ -73,140 +72,13 @@ const getbookshop = (_, resp) => {
     });
 };
 
-
-
-
-// const signIn = async (req, resp) => {
-//   MongoClient.connect(url, function (err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("mydb");
-//     dbo.createCollection("customers", function (err, res) {
-//       if (err) throw err;
-//       console.log("Collection created!");
-//       db.close();
-//     });
-//   });
-//   console.log(MongoClient);
-// };
-
-// const logIn = async (req, resp) => {
-//     var map = req.body;
-//     console.log(map.email);
-
-//     MongoClient.connect(url, async (error, dbI) => {
-//         var person = dbI
-//             .db("bkim")
-//             .collection("post")
-//             .findOne({ email: map.email });
-
-//         console.log(person);
-
-//     });
-// };
-
-// const uploadText = (req, resp) => {
-//     var map = req.body;
-//     console.log(map)
-//     var dateOfPost = new Date().toLocaleString();
-//     var obj = {
-//         title: map.title,
-//         post: map.info,
-//         name: map.name,
-//         date: dateOfPost,
-//     };
-//     client.connect( async (error, dbI) => {
-//         if (error) console.log(error);
-//         var db = dbI.db("posts");
-//         await db.collection("messeges").insertOne(obj);
-
-//         console.log("saved");
-//     });
-
-//     resp.send("uploaded");
-//     resp.end();
-// };
-
-// const uploadImage = (req, resp) => {
-//     var map = req.body;
-//     console.log(map)
-//     var dateOfPost = new Date().toLocaleString();
-//     var obj = {
-//         title: map.title,
-//         post: map.info,
-//         name: map.name,
-//         date: dateOfPost,
-//     };
-//     client.connect( async (error, dbI) => {
-//         if (error) console.log(error);
-//         var db = dbI.db("posts");
-//         await db.collection("messeges").insertOne(obj);
-
-//         console.log("saved");
-//     });
-
-//     resp.send("uploaded");
-//     resp.end();
-// };
-
-// const uploadVideo = (req, resp) => {
-//     var map = req.body;
-//     console.log(map)
-//     var dateOfPost = new Date().toLocaleString();
-//     var obj = {
-//         title: map.title,
-//         post: map.info,
-//         name: map.name,
-//         date: dateOfPost,
-//     };
-//    client.connect(async (error, dbI) => {
-//         if (error) console.log(error);
-//         var db = dbI.db("posts");
-//         await db.collection("messeges").insertOne(obj);
-
-//         console.log("saved");
-//     });
-
-//     resp.send("uploaded");
-//     resp.end();
-// };
-
-// const getInfo = (req, resp) => {
-//     map = req.body
-//     console.log(map);
-
-//     client.connect((error, dbI) => {
-//         if (error) console.log(error);
-//         var dbForPosts = dbI.db("posts");
-//         // var postedBy = dbI.db("bkim").collection('posts').findOne({ email: map.email }).name;
-//         // console.log(postedBy)
-//         // if (postedBy == null || postedBy == undefined) {
-//         //     resp.send('unauthorised access! Sign in first to access this data.')
-//         //     return
-//         // }
-//         dbForPosts.collection("items")
-//             .find({}).toArray(async (error, result) => {
-//                 if (error) {
-//                     console.log(error);
-//                 }
-//                 await resp.json(result.reverse());
-//                 await resp.end();
-//             });
-//     });
-// }
-
 const listen = () => {
   console.log(`server listening on port: ${require("./port.js").c110}`);
 };
 
 module.exports = {
   homepage,
-  // uploadText,
-  // uploadImage,
-  // uploadVideo,
-  // getInfo,
   listen,
   getict,
   getbookshop,
-  // logIn,
-  // signIn,
 };
